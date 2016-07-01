1c918c1f4c34fc8939591c9963ac23f7f623b389
catkic <476646484@qq.com>
2016-07-01 16:35:24 +0800

    url 规则的小修改，使之更符合需求

--- upload/urls.py
+++ upload/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index', views.index, name='index'),
+    url(r'^$', views.list, name='list'),
     url(r'^list$', views.list, name='list'),
     url(r'^test$', views.test, name='test'),
     url(r'^api', views.api, name='api'),
